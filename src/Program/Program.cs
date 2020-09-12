646a392c808c38a5de424a7bc08a6093d292e91a
GozCorrea <62217597+GozCorrea@users.noreply.github.com>
2020-09-12 20:01:38 -0300

    fix changes

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,7 +7,9 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            VigenereCode v = new VigenereCode("ucu", 8);
+            VigenereCode v = new VigenereCode("ucu", 0);
+            Console.WriteLine(v.Encrypt("PROGRAMACIONII"));
+
             Console.WriteLine(v.Decrypt("ISVJED"));
             Console.WriteLine(v.Decrypt("WMWFKN"));
             Console.WriteLine(v.Decrypt("TORQ"));
